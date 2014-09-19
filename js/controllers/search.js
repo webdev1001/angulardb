9bf3a94b850784b9844df7fabd6c40c0eecc8ae4
Aaron John Schlosser <aaron@aaronschlosser.com>
2014-09-19 13:59:52 -0700

    meh

--- js/controllers/search.js
+++ js/controllers/search.js
@@ -29,10 +29,10 @@ uiControllers.controller("searchController", function ($scope, $filter) {
 		$scope.quantity += n;
 		$scope.results = 0;
 	};
-	$scope.sort = function (predicate) {
+	$scope.sort = function (by) {
 		var x = $scope.clients.toBeReversed;
 		$scope.clients.sort(function (a,b) {
-			if (predicate === "name") {
+			if (by === "name") {
 				if (x) {
 					$scope.clients.toBeReversed = false;
 					return a.name.toLowerCase() < b.name.toLowerCase() ? 1 : -1;
@@ -41,7 +41,7 @@ uiControllers.controller("searchController", function ($scope, $filter) {
 					return a.name.toLowerCase() > b.name.toLowerCase() ? 1 : -1;
 				}
 			}
-			else if (predicate === "id") {
+			else if (by === "id") {
 				if (x) {
 					$scope.clients.toBeReversed = false;
 					return a.id - b.id;
@@ -50,7 +50,7 @@ uiControllers.controller("searchController", function ($scope, $filter) {
 					return b.id - a.id;
 				}
 			}
-			else if (predicate === "sites") {
+			else if (by === "sites") {
 				if (x) {
 					$scope.clients.toBeReversed = false;
 					return a.sites.length - b.sites.length;
